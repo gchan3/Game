52382540217fbc7f136fdd533f97d31202a7f87d
Tkang5000 <36413214+Tkang5000@users.noreply.github.com>
2018-06-14 10:51:34 -0400

    Create imagelocations.java

---
+++ imagelocations.java
@@ -0,0 +1,7 @@
+//Icons
+	image(autoClickerImage, 201, 51, 69, 75);
+	image(grandmaImage, 201, 131, 69, 75);
+	image(factoryImage, 201, 211, 69, 74);
+	image(mineImage, 201, 291, 69, 75);
+	image(labImage, 201, 370, 70, 75);
+	image(portalImage, 201, 450, 69, 75);
